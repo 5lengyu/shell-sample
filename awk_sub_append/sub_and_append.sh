bcc46c726560825e41849a9f69e7961b38fcdb2e
Joe Li <joe.li@coupang.com>
2019-03-12 15:40:16 +0800

    the target file should be a json, modify that

--- awk_sub_append/sub_and_append.sh
+++ awk_sub_append/sub_and_append.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # parameter 1 is a csv file with 7 columns
-# parameter 2 is the target file path
+# parameter 2 is the target json file path
 # what we want is to extract the 1st 2nd 7th columns and append a date '2088-01-01 00:00:00' in the end of every line
 # use NR!=1 to skip the column header
 
