c4ae926d5bdfb80e09929d72e3e805ff1d075992
Joe Li <joe.li@coupang.com>
2019-05-09 09:59:44 +0800

    CLI to download oracle jdk8

---
+++ compilation.sh
@@ -0,0 +1,4 @@
+
+# Only this command works, no idea why, and only this url works, if you replace the url with your version jdk url, it doesn't work.
+# To download oracle jdk via CLI
+curl -L -C - -b "oraclelicense=accept-securebackup-cookie" -O https://download.oracle.com/otn/java/jdk/8u211-b12/478a62b7d4e34b78b671c754eaaf38ab/jdk-8u211-linux-x64.tar.gz
